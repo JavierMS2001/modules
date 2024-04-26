cdaa8b8fb32ddd2026099c08626ba35261cc000f
JavierMS2001 <franciscojavierms2001@gmail.com>
2024-04-26 09:12:36 +0000

    Arreglo del modulo pokeinfo

--- pokeinfo/plugins/modules/pokeinfo.py
+++ pokeinfo/plugins/modules/pokeinfo.py
@@ -2,29 +2,17 @@
 from ansible.module_utils.basic import AnsibleModule
 import requests
 
-def get_pokemon_data(pokemon_name, fields=None):
+def get_pokemon_data(pokemon_name):
     api_url = f"https://pokeapi.co/api/v2/pokemon/{pokemon_name.lower()}"
     response = requests.get(api_url)
     if response.status_code == 200:
-        data = response.json()
-        pokemon_info = {
-            "name": data["name"],
-            "id": data["id"],
-            "types": [t["type"]["name"] for t in data["types"]],
-            "stats": {stat["stat"]["name"]: stat["base_stat"] for stat in data["stats"]}
-        }
-        if fields:
-            filtered_data = {field: pokemon_info.get(field) for field in fields}
-            return filtered_data
-        else:
-            return pokemon_info
+        return response.json()
     else:
         return None
 
 def main():
     module_args = dict(
-        name=dict(type='str', required=True),
-        fields=dict(type='list', default=None)
+        name=dict(type='str', required=True)
     )
 
     result = dict(
@@ -39,15 +27,13 @@ def main():
     )
 
     pokemon_name = module.params['name']
-    fields = module.params['fields']
-
-    pokemon_data = get_pokemon_data(pokemon_name, fields)
+    pokemon_data = get_pokemon_data(pokemon_name)
 
     if pokemon_data:
         result['pokemon_data'] = pokemon_data
         module.exit_json(**result)
     else:
-        result['error'] = f"Pokemon with name: '{pokemon_name}' dont found."
+        result['error'] = f"Pokemon '{pokemon_name}' not found"
         module.fail_json(msg="Failed to fetch Pokemon data", **result)
 
 if __name__ == '__main__':
